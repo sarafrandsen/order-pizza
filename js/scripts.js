b088024da7b3837e1d2a8a55ce488f29dff23cdd
Sara <sara.frandsen@gmail.com>
2017-01-27 11:41:31 -0800

    experiment with checkboxes on new branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,16 @@
-//business-logic
-function Pizza(size) {
-  this.size = size;
-  this.toppings = [];
-};
-
-Pizza.prototype.chooseSize = function() {
-
-};
-
-Pizza.prototype.chooseTopping = function() {
-
-};
-
-Pizza.prototype.calculatePrice = function() {
-
-};
-
+// //business-logic
+// function Pizza(size, toppings, price) {
+//   this.pizzaSize = size;
+//   this.toppings = 0;
+//   this.price = 0;
+// };
+//
+// Pizza.prototype.calculatePrice = function() {
+//   array.forEach(function() {
+//     this.pizzaSize += this.toppings[]
+//   };
+// };
+//
 
 //front-end logic
 $(document).ready(function() {
@@ -23,7 +18,20 @@ $(document).ready(function() {
     event.preventDefault();
 
     var sizeChoice = parseInt($('input[name="size"]:checked', '#selections-made').val());
+    // var toppingChoice = 0;
 
-    $("#list-pizza-selections").text(sizeChoice);
+    var toppingInput = parseInt($("input:checkbox[name=topping]:checked", "#selections-made").length);
+    if(toppingInput > 0) {
+      return toppingInput * 2;
+    } else {
+      return 0;
+    };
+    console.log(toppingInput);
+
+
+    // var pizzaPrice = new Pizza(sizeChoice);
+
+    $("#final-pizza-size").text(sizeChoice);
+    $("#final-pizza-topping").text(toppingInput);
   });
 });
